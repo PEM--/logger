400b15a0a7c315c3ef97e0ef210abed875dc81f4
PEM-- <pemarchandet@gmail.com>
2015-09-30 20:15:49 +0200

    0.0.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,2 @@
-* 0.0.2: Remove React dependency and update Browserify
+* 0.0.2/0.0.3: Remove React dependency and update Browserify
 * 0.0.1: Initial commit
--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'pierreeric:logger',
-  version: '0.0.2',
+  version: '0.0.3',
   summary: 'Isomorphic logger',
   git: 'https://github.com/PEM--/logger',
   documentation: 'README.md'
